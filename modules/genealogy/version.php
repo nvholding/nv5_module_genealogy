929d8c3d0ef12d01897a97ef3454526d09a6e521
webvang <hoang.nguyen@webvang.vn>
2020-01-21 17:36:54 +0700

    fix version

--- modules/genealogy/version.php
+++ modules/genealogy/version.php
@@ -17,7 +17,7 @@ $module_version = array(
 	'submenu' => 'content,rss,search',
 	'is_sysmod' => 0, // 1:0 => Co phai la module he thong hay khong
 	'virtual' => 1, // 1:0 => Co cho phep ao hao module hay khong
-	'version' => '4.0.0', // Phien ban cua modle
+	'version' => '5.0.0', // Phien ban cua modle
 	'date' => 'Wed, 01 Jan 2020 00:00:00 GMT', // Ngay phat hanh phien ban
 	'author' => 'NV Holding (ceo@nvholding.vn)', // Tac gia
 	'note' => '', // Ghi chu
